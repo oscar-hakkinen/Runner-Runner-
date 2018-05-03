2ff56d28a786426bdba2cfdf7a585cee48616b5a
oscar-hakkinen <Hako1_15@uni.worc.ac.uk>
2018-05-03 18:22:02 +0100

    button math tests test

--- www/js/index.js
+++ www/js/index.js
@@ -7,6 +7,51 @@ var map;
 var loc;
 var path = [];
 
+var foodscript = 1;
+var barricadescript = 1;
+var armoryscript = 1;
+var scrapscript = 1;
+var resourcescript = 1;
+                        
+                     $(document).ready(function(){
+    
+                     $("#foodUpgrade").click(function(){
+                     document.getElementById("foodAmount").innerHTML = "Food:" +foodscript + "/100";
+                        if(foodscript<100){
+                        foodscript++;
+                        }     
+                    });
+                         
+                    $("#barricadeUpgrade").click(function(){
+                     document.getElementById("barricadeAmount").innerHTML = "Barricades:" +barricadescript + "/100";
+                        if(barricadescript<100){
+                        barricadescript++;
+                        }     
+                    });   
+                         
+                          
+                    $("#armoryUpgrade").click(function(){
+                     document.getElementById("armoryAmount").innerHTML = "Armory:" + armoryscript + "/100";
+                        if(armoryscript<100){ 
+                       armoryscript++;
+                        }     
+                    });  
+                         
+                  $("#scrapUpgrade").click(function(){
+                     document.getElementById("scrapAmount").innerHTML = "Scrap:" + scrapscript + "/100";
+                        if(scrapscript<100){ 
+                       scrapscript++;
+                        }     
+                    });
+                          $("#resourceUpgrade").click(function(){
+                     document.getElementById("resourceAmount").innerHTML = "Resources:" +resourcescript;
+                        if(resourcescript<100){
+                        resourcescript++;
+                        }     
+                    });
+                });
+    
+
 
     
 
