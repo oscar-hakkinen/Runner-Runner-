0ca94f3a91038cb5a21519edfcae9b20575ce214
oscar-hakkinen <Hako1_15@uni.worc.ac.uk>
2018-05-04 14:38:37 +0100

    did it?

--- www/js/index.js
+++ www/js/index.js
@@ -7,10 +7,10 @@ var map;
 var loc;
 var path = [];
 
-var prevLat = 1;
-var prevLong = 2;
-var currentlat = 5;
-var currentLong = 6;
+var prevLat;
+var prevLong;
+var currentlat;
+var currentLong;
 
 
 var foodscript = 0;
@@ -67,14 +67,7 @@ var mapresourcescript = 1;
                         resourcescript++;
                         }     
                     });
-                            $("#mapResourceUpgrade").click(function(){
-                     document.getElementById("mapResourceAmount").innerHTML = "Resources:" + mapresourcescript;
-                                 document.getElementById("resourceAmount").innerHTML = "Resources:" + resourcescript;      
-                        if(mapresourcescript<400){
-                        mapresourcescript++;
-                        resourcescript++;    
-                        }     
-                    });
+                           
                 });
 
  $(document).on( "pageinit", "#pagethree", function() {
@@ -103,6 +96,41 @@ function onLocationSuccess(position) {
     
     //work out distance between prev and current
     //only if prev has a value
+
+ $("#mapResourceUpgrade").click(function(){
+                     document.getElementById("mapResourceAmount").innerHTML = "Resources:" + mapresourcescript;
+                                 document.getElementById("resourceAmount").innerHTML = "Resources:" + resourcescript;      
+                        if(mapresourcescript<400){
+                        mapresourcescript++;
+                        resourcescript++;    
+                           
+    alert(getDistanceFromLatLonInKm(prevLat,prevLong,currentlat,currentLong).toFixed(2));    
+                        }     
+                    });
+
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  
+    var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+    
+    
+    
+    
     
     
     position.coords.latitude, position.coords.longitude
@@ -140,30 +168,6 @@ function onLocationSuccess(position) {
 }
 
 
-
-
-alert(getDistanceFromLatLonInKm(prevLat,prevLong,currentlat,currentLong).toFixed(2));
-
-
-
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-  
-    var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
-  return d;
-}
-
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
-}
 
 
 
